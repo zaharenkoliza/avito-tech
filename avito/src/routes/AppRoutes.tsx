0a593eb39f2c7e285c27ad1a0c01048abbddc469
zaharenkoliza <Liza93087@gmail.com>
2025-02-15 16:30:36 +0300

    router

---
+++ avito/src/routes/AppRoutes.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from '../pages/Home';
+import Request from '../pages/Request';
+import List from '../pages/List';
+import AdPage from '../pages/AdPage';
+
+const AppRoutes: React.FC = () => {
+	return (
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/form" element={<Request />} />
+			<Route path="/list" element={<List />} />
+			<Route path="/item/:id" element={<AdPage />} />
+			{/* <Route path="/item" element={<Request />} /> */}
+		</Routes>
+	);
+};
+
+export default AppRoutes;
